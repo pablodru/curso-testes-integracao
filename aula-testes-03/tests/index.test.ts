58fa8e00ff1e5b26604562202444c7f2ebf27ebc
pablodru <pdrocha03@gmail.com>
2023-09-26 17:41:07 -0300

    aula-testes-integracao 01/02/03

---
+++ aula-testes-03/tests/index.test.ts
@@ -0,0 +1,14 @@
+describe("aula-testes-03", () => {
+    it("should return the sum of twe values", ()=>{
+        expect(2+2).toBe(4);
+    })
+    it("should return the difference of twe values", ()=>{
+        expect(2-2).toBe(0);
+    })
+    it("should return the multiplication of twe values", ()=>{
+        expect(2*2).toBe(4);
+    })
+    it("should return the division of twe values", ()=>{
+        expect(2/2).toBe(1);
+    })
+})
